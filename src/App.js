5d63a0004274ed1830b2b0c3a1b8517b4fd609db
Maciej Mlynowski <szypki@gmail.com>
2023-03-15 20:59:31 +0100

    Fix app.js and cosmetic change in Timer definition

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import Timer from './Components/Timer/Timer';
 const App = () => {
   const [time, setTime] = useState(0);
   const [timer, setTimer] = useState(null);
-  const [timeOn, setTimeOn] = useState(false);
 
   const startT = () => {
-    setTimeOn(true);
+    if (!timer) {
+      setTimer(
+        setInterval(() => {
+          setTime((prevValue) => prevValue + 1);
+        }, 1)
+      );
+    }
   };
 
   const stopT = () => {
-    setTimeOn(false);
+    clearInterval(timer);
+    setTimer(null);
   };
 
   const resetT = () => {
@@ -21,20 +27,10 @@ const App = () => {
   };
 
   useEffect(() => {
-    if (timeOn) {
-      setTimer(
-        setInterval(() => {
-          setTime((prevValue) => prevValue + 10);
-        }, 10)
-      );
-    } else {
-      clearInterval(timer);
-    }
-
     return () => {
       if (timer) clearInterval(timer);
     };
-  }, [timeOn]);
+  }, [timer]);
 
   return (
     <div className='timer'>
--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -3,8 +3,8 @@ import styles from './Timer.module.scss';
 const Timer = ({ miliseconds }) => {
   const ms = miliseconds % 1000;
   const ss = Math.floor((miliseconds / 1000) % 60);
-  const mm = Math.floor((miliseconds / 1000 / 60) % 60);
-  const hh = Math.floor((miliseconds / 1000 / 60 / 60) % 24);
+  const mm = Math.floor((miliseconds / (60 * 1000)) % 60);
+  const hh = Math.floor((miliseconds / (60 * 60 * 1000)) % 24);
 
   function formatedTime(num, targetLength) {
     return num.toString().padStart(targetLength, '0');
